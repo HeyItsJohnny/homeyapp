4629189d517eaa1cd0c7aeafb1963873b00f560b
Jonathan Laroco <jonathan.laroco@gmail.com>
2023-04-12 21:43:29 -0700

    Clean up

--- src/index.js
+++ src/index.js
@@ -4,11 +4,4 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-/*
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-*/
+
