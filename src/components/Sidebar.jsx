d3d4cfb40ba7a75de91ea13bca4fac29d25ade4c
Jonathan Laroco <jonathan.laroco@gmail.com>
2023-04-13 22:05:09 -0700

    Change Title

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -24,7 +24,7 @@ const Sidebar = () => {
               onClick={() => {}}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
-              <SiShopware /> <span>Shoppy</span>
+              <SiShopware /> <span>Homey App</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
