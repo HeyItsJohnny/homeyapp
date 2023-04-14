796f34313870a0d508ea95a4baa7ab104ccf9177
Jonathan Laroco <jonathan.laroco@gmail.com>
2023-04-14 15:18:15 -0700

    Adding NAV Bar

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -4,18 +4,28 @@ const StateContext = createContext();
 
 const initialState = {
   chat: false,
-  cart: false,
   userProfile: false,
   notification: false,
 };
 
 export const ContextProvider = ({ children }) => {
-  const [activeMenu, setActiveMenu] = useState(true);
+  const [activeMenu, setActiveMenu] = useState(true); 
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true});
+  } 
 
   return (
     <StateContext.Provider value={{ 
         activeMenu, 
-        setActiveMenu
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize
     }}>
       {children}
     </StateContext.Provider>
